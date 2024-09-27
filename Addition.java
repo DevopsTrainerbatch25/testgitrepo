4df31b25112021b130bf53a19cf2936d58433306
DevopsTrainerbatch25 <gcpdevopsbatch25@gmail.com>
2024-09-27 09:40:43 +0530

    Added src folder and files

---
+++ Addition.java
@@ -0,0 +1,11 @@
+class Addition
+{
+	public static void main(String args[])
+	{
+		int num1,num2,res;
+		num1=10;
+		num2=25;
+		res=num1+num2;
+		System.out.println("SUm of "+num1+" and "+num2+" is "+res);
+	}
+
