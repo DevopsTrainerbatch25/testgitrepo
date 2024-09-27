d5e6c2b9ad7c4b6ebef49da318b69f5820a7c56e
DevopsTrainerbatch25 <gcpdevopsbatch25@gmail.com>
2024-09-27 10:25:43 +0530

    Updated SOP statement

--- Addition.java
+++ Addition.java
@@ -7,7 +7,7 @@ class Addition
 		num2=250;
 		res=num1+num2;
 		mul=num1*num2;
-		System.out.println("SUm of "+num1+" and "+num2+" is "+res);
+		System.out.println("Addition of "+num1+" and "+num2+" is "+res);
 		System.out.println("Prodyct of "+num1+" and "+num2+" is "+mul);
 	}
 
