c733bb1b7e5153025ee1b2cda87b2dc8a9d24ca9
DevopsTrainerbatch25 <gcpdevopsbatch25@gmail.com>
2024-09-27 09:57:10 +0530

    input values changed

--- Addition.java
+++ Addition.java
@@ -3,8 +3,8 @@ class Addition
 	public static void main(String args[])
 	{
 		int num1,num2,res,mul;
-		num1=10;
-		num2=25;
+		num1=100;
+		num2=250;
 		res=num1+num2;
 		mul=num1*num2;
 		System.out.println("SUm of "+num1+" and "+num2+" is "+res);
